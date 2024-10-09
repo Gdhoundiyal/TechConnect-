8909fc5405d876a5458325b14b34dc7024ece16e
Gdhoundiyal <gdhoundiyal2487@gmail.com>
2024-10-09 16:49:49 +0530

    hospital/ecommerce data modeling

---
+++ backend/models/hospital-mangement/hospital.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const hospitalSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        dignosedWith:{
+            type: String,
+            required: true
+        },
+        addressLine1:{
+            type: String,
+            required: true
+        },
+        addressLine1:{
+            type: String,
+        },
+        city:{
+            type: String,
+            required: true
+        },
+        pincode:{
+            type: String,
+            required: true
+        },
+        specializedIn:[
+            {
+            type: String,
+            }
+        ],
+    },
+    {timestamps: true}
+)
+
+export  const Hospital = mongoose.model("Hospital", hospitalSchema)
